cea102bdb9a602bff56a111ca73d8b2132b2019f
PMolnes <pettermolnes@icloud.com>
2022-02-10 13:10:40 +0100

    Removed unused imports.

--- src/main/java/ntnu/appdev/jdbcconnection/ProjectController.java
+++ src/main/java/ntnu/appdev/jdbcconnection/ProjectController.java
@@ -4,10 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
